ccef19fb1a38034f1e671b170370bc2f9c79bb52
ZJT8848 <1137285023@qq.com>
2025-11-06 20:04:30 +0800

    Add files via upload

--- openstack.sh
+++ openstack.sh
@@ -473,8 +473,8 @@ key_repository = /etc/keystone/fernet-keys/
 key_repository = /etc/keystone/credential-keys/
 [auth]
 methods = password,token
-password = <PASSWORD>
-token = <TOKEN>
+password = keystone.auth.plugins.password.Password
+token = keystone.auth.plugins.token.Token
 [endpoint_filter]
 driver = sql
 [identity]
